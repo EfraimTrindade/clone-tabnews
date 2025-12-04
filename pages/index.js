4edcfd73b46486a6fa3924201319adbba0f0a8e8
Efraim Trindade <118997788+EfraimTrindade@users.noreply.github.com>
2025-12-04 19:57:13 +0000

    adiciona scripts `link:check` e  `link:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
-function Home(){
-    return <h1>Todo problema complexo é apenas uma sequência de passos lógicos esperando para ser desvendada.</h1>
+function Home() {
+  return (
+    <h1>
+      Todo problema complexo é apenas uma sequência de passos lógicos esperando
+      para ser desvendada.
+    </h1>
+  );
 }
 
 export default Home;
